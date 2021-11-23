a91517ed4b7f2b73c82ee5130c73482f6186aa00
Yevhenii Horbenko <yevhenii.horbenko@gmail.com>
2021-11-23 22:49:42 +0200

    use home directory instead of full path

--- .fzf.zsh
+++ .fzf.zsh
@@ -1,13 +1,13 @@
 # Setup fzf
 # ---------
-if [[ ! "$PATH" == */Users/yevheniihorbenko/.fzf/bin* ]]; then
-  export PATH="${PATH:+${PATH}:}/Users/yevheniihorbenko/.fzf/bin"
+if [[ ! "$PATH" == ~/.fzf/bin* ]]; then
+  export PATH="~/.fzf/bin"
 fi
 
 # Auto-completion
 # ---------------
-[[ $- == *i* ]] && source "/Users/yevheniihorbenko/.fzf/shell/completion.zsh" 2> /dev/null
+[[ $- == *i* ]] && source "~/.fzf/shell/completion.zsh" 2> /dev/null
 
 # Key bindings
 # ------------
-source "/Users/yevheniihorbenko/.fzf/shell/key-bindings.zsh"
+source "~/.fzf/shell/key-bindings.zsh"
